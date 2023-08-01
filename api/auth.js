32b6bbc01b6bebd0bac3c43c3cb00b0d247120cd
AnantParmar <adp2002in@gmail.com>
2023-08-02 00:45:40 +0530

    backend

--- api/auth.js
+++ api/auth.js
@@ -28,7 +28,7 @@ router.use(bodyParser.json());
 // router.use(bodyParser.);
 
 router.post('/uploadPic',upload.single("profile"), async (req,res)=>{
-    res.setHeader("Access-Control-Allow-Origin","https://jigarii-frontend.vercel.app")
+    res.setHeader("Access-Control-Allow-Origin","*")
     try {
         const dateTime = giveCurrentDateTime();
         const storageRef = ref(storage, `file/${req.file.originalname+" "+dateTime}`);
@@ -67,7 +67,7 @@ const giveCurrentDateTime = () => {
     return dateTime;
 }
 router.post('/signup', async (req,res)=>{
-    res.setHeader("Access-Control-Allow-Origin","https://jigarii-frontend.vercel.app")
+    res.setHeader("Access-Control-Allow-Origin","*")
     await createUserWithEmailAndPassword(auth,req.body.email,req.body.password)
     .then(async (response) => {
       
@@ -100,7 +100,7 @@ function generateSessionId(userId) {
     return hash;
   }              
 router.post('/login', async (req, res)=>{
-    res.setHeader("Access-Control-Allow-Origin","https://jigarii-frontend.vercel.app")
+    res.setHeader("Access-Control-Allow-Origin","*")
     
     const email = req.body.username;
     const password = req.body.password;
@@ -138,7 +138,7 @@ router.post('/login', async (req, res)=>{
 })
 
 router.post('/getUser', async (req,res)=>{
-    res.setHeader("Access-Control-Allow-Origin","https://jigarii-frontend.vercel.app")
+    res.setHeader("Access-Control-Allow-Origin","*")
     res.send(auth.currentUser)
 })
 module.exports = router;
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cookieParser());
  app.use(bodyParser.urlencoded({ extended: false }))
 //  app.use(cors())
  app.use(cors({
-  origin: 'https://jigarii-frontend.vercel.app'
+  origin: '*'
 }));
  app.use('/profile', express.static('upload/img'));
  app.get('/', (req, res)=>{
