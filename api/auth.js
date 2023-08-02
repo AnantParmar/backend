731c81bd3d6b4e3b1630bdfe9c13fc9e98ea1c9e
AnantParmar <adp2002in@gmail.com>
2023-08-02 11:54:55 +0530

    backend

--- api/auth.js
+++ api/auth.js
@@ -121,6 +121,7 @@ router.post('/login', async (req, res)=>{
             else {
                 res.cookie("sessionID", '', { expires: new Date(0), httpOnly: true });
                 res.cookie("result","cookieRemoved", {httpOnly:true})
+                res.cookie("sessionId",sessionId, {httpOnly:true});
             }
 
             const q1 = query(collection(db, "likedByUser"), where("user", "==", response.user.uid));
