4d0d60a15bece044c1da1bca94fe97d9c45ffa0a
AnantParmar <adp2002in@gmail.com>
2023-08-12 00:07:31 +0530

    auth change

--- api/auth.js
+++ api/auth.js
@@ -127,9 +127,7 @@ router.post('/login', async (req, res)=>{
             const customToken = await admin.auth().createCustomToken(response.user.uid);
             console.log(customToken)
 
-            res.cookie('sessionId',"customToken xyz", {
-                sameSite: 'none'
-            });
+            res.cookie('sessionId',"customToken xyz");
             // res.setHeader('set-cookie', "a=b");
             const q1 = query(collection(db, "likedByUser"), where("user", "==", response.user.uid));
             const quote = await getDocs(q1);
