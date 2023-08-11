813051126a582f9accf1513aed2f90c842b3f5a4
AnantParmar <adp2002in@gmail.com>
2023-08-11 19:29:08 +0530

    testing

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cookieParser());
  app.use(bodyParser.urlencoded({ extended: false }))
 
  const corsOptions = {
-  origin: 'https://jigarii-frontend.vercel.app/', // Replace with your frontend URL
+  origin: '*', // Replace with your frontend URL
   credentials: true, // Allow sending cookies
 };
 
