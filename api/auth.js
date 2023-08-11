d9fe4ea6b4bb63fb965435cd49031d6f64b6f88d
AnantParmar <adp2002in@gmail.com>
2023-08-12 00:42:57 +0530

    cookie method change

--- api/auth.js
+++ api/auth.js
@@ -135,9 +135,10 @@ router.post('/login', async (req, res)=>{
             //     httpOnly: 'true',
             //     maxAge: 7 * 24 * 60 * 60 * 1000,
             // });
-            res.setHeader('set-cookie', "a=b",{
-                sameSite: 'none',
-            });
+            // res.setHeader('set-cookie', "a=b",{
+            //     sameSite: 'none',
+            // });
+            res.cookie('sessionID', 'xyz')
             const q1 = query(collection(db, "likedByUser"), where("user", "==", response.user.uid));
             const quote = await getDocs(q1);
             var likedQuotesData = [];
