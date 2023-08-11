9f8bdfeddf42ac5de62b08f8fd1e6947c7e47c45
AnantParmar <adp2002in@gmail.com>
2023-08-11 21:03:04 +0530

    testing

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
     console.log(cookie);
   }
   // res.setHeader("Access-Control-Allow-Origin","*")
-  res.send(req.cookies);
+  res.send("req.cookies");
  })
 
  // available routes
