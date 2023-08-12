8039e8d394d33c53eeb5403129b91bee4451958c
AnantParmar <adp2002in@gmail.com>
2023-08-12 13:04:37 +0530

    change in quote

--- api/quote.js
+++ api/quote.js
@@ -80,7 +80,7 @@ router.get('/getQuotes', async (req,res) => {
     res.setHeader("Access-Control-Allow-Credentials", "true");
     console.log('quotes '+req.cookies.random)
     // res.cookie('random', 'xyz')
-    res.setHeader('Set-Cookie', "random=xyz")
+    res.setHeader('Set-Cookie', 'random=xyz; Secure; HttpOnly')
     // res.cookie('random', 'xyz', {
     //     sameSite: 'none',
     //     secure: 'false'
